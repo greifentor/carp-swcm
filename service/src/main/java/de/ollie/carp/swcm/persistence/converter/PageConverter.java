eb5c3740bb981ca3d7def730953ac2199f2a47ee
ollie <lieshoff@gmx.de>
2021-07-28 23:22:01 +0200

    Check in after first compile clean code generation.

--- service/src/main/java/de/ollie/carp/swcm/persistence/converter/PageConverter.java
+++ service/src/main/java/de/ollie/carp/swcm/persistence/converter/PageConverter.java
@@ -2,21 +2,27 @@ package de.ollie.carp.swcm.persistence.converter;
 
 import java.util.stream.Collectors;
 
+import javax.inject.Named;
+
 import de.ollie.carp.swcm.core.model.Page;
+
 import lombok.AllArgsConstructor;
+import lombok.Generated;
 
 /**
  * A class to convert a repository page to a service layer page object.
- * 
+ *
  * @param <CONTENT> The type of the page content.
  * @param <DBO>     The type of the DBO's which are representing CONTENT objects in the persistence layer.
  *
- *                  GENERATED CODE !!! DO NOT CHANGE !!!
+ * GENERATED CODE !!! DO NOT CHANGE !!!
  */
+@Generated
 @AllArgsConstructor
+@Named
 public class PageConverter<CONTENT, DBO> {
 
-	private ToModelConverter<CONTENT, DBO> toModelConverter;
+	private final ToModelConverter<CONTENT, DBO> toModelConverter;
 
 	public Page<CONTENT> convert(org.springframework.data.domain.Page<DBO> page) {
 		if (page == null) {
--- service/src/main/java/de/ollie/carp/swcm/persistence/converter/ToModelConverter.java
+++ service/src/main/java/de/ollie/carp/swcm/persistence/converter/ToModelConverter.java
@@ -4,9 +4,9 @@ package de.ollie.carp.swcm.persistence.converter;
  * An interface for converter which are able to convert DBO to model objects.
  *
  * @param <MODEL> The type of the model objects.
- * @param <DBO>   The type of the DBO's which are representing CONTENT objects in the persistence layer.
+ * @param <DBO>   The type of the DBO's which are representing MODEL objects in the persistence layer.
  *
- *                GENERATED CODE !!! DO NOT CHANGE !!!
+ * GENERATED CODE !!! DO NOT CHANGE !!!
  */
 public interface ToModelConverter<MODEL, DBO> {
 
